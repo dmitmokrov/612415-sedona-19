171da1fa50b0d31c348f0d587263d44ce369b81c
Dmitry Mokrov <dmit.mokrov@yandex.ru>
2020-04-26 19:57:09 +0300

    Оптимизирует загрузку шрифтов, минимизирует html и скрипты, исправляет верстку страницы формы в firefox

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,16 @@
+var menuToggler = document.querySelector(".main-nav__toggle");
+var menu = document.querySelector(".main-nav__list");
+
 svg4everybody();
+
+// Открытие/закрытие меню в мобильной версии
+
+menuToggler.addEventListener("click", function () {
+  if (menu.classList.contains("js-show")) {
+    menu.classList.remove("js-show");
+    this.classList.remove("js-toggler-show");
+  } else {
+    menu.classList.add("js-show");
+    this.classList.add("js-toggler-show");
+  }
+});
